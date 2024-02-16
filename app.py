b361d5bc7f74c4f16dfb1cc648315996533c711f
ZEYNEP2121 <90401513+ZEYNEP2121@users.noreply.github.com>
2024-02-16 12:57:23 +0000

    I did dynamic data and cloud deployment

--- app.py
+++ app.py
@@ -1,9 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
+
+JOBS=[
+  {'id': 1,
+   'title':'Data Analyst',
+   'location':'Bengaluru, India',
+   'salary':'Rs. 10,00,000'
+  },
+  {'id': 2,
+   'title':'Data Scientist',
+   'location':'Delhi, India',
+   'salary':'Rs. 15,00,000'
+  },
+  {'id': 3,
+   'title':'Front-end Engineer',
+   'location':'Remote'
+  },
+  {'id': 4,
+   'title':'Back-end Engineer',
+   'location':'La, Usa',
+   'salary':'$120,000'
+  }
+]
+
+
+
 @app.route('/')
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',
+                         jobs=JOBS)
+
+@app.route("/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__== '__main__':
